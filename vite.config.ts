497a3bccfe78072c867ae94c205544d1bd12687e
roadsterdev <roadsterdev@gmail.com>
2024-08-09 11:16:52 +0300

    add logo and install react-query, update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true
-  }
-})
-
-
+    host: true,
+  },
+  optimizeDeps: {
+    include: ["rc-pagination"],
+  },
+});
